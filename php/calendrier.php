65515cdcd310ef23c55ce250939f250f32ba4dfe
JeanFabry <jeanfabry1435@gmail.com>
2021-01-14 18:44:49 +0100

    final

--- php/calendrier.php
+++ php/calendrier.php
@@ -61,6 +61,57 @@ try {
         </div>
         <div class="contenaireEvetButton">
             <button class="addEventButton">Add a recipe</button>
+            <?php
+            // Recupéré session de calendrier
+            $req = $bdd->prepare('SELECT id_user FROM Students WHERE email = ?');
+            $req->execute([
+                $_SESSION['email']
+            ]);
+            $data = $req->fetch();
+
+            $req->closeCursor();
+
+            $_SESSION['idUser'] = $data['id_user'];
+
+            if (!empty($_POST['title_watch']) && !empty($_POST['date']) && !empty($_POST['description'])) {
+                $req = $bdd->prepare('INSERT INTO watch_recipe (FK_id_user, title_watch, date, description) VALUES (?, ?, ?, ?)') or die(print_r($bdd->errorInfo()));
+                $req->execute([
+                    $_SESSION['idUser'],
+                    strip_tags(trim($_POST['title_watch'])),
+                    strip_tags(trim($_POST['date'])),
+                    strip_tags(trim($_POST['description'])),
+                ]);
+                $req->closeCursor();
+            ?>
+
+                <h1> C'est encoder ;-)</h1>
+
+            <?php
+
+            } else {
+            ?>
+
+                <div class="form-popup" id="myForm" style="display:none">
+                    <form class="formContent" action="" method="POST">
+                        <h1>Add a watch</h1>
+
+                        <label for="email">Title</label>
+                        <input type="text" placeholder="Title" name="title_watch">
+
+                        <label for="date">Date</label>
+                        <input type="date" placeholder="Enter Password" name="date">
+
+                        <label for="explication">Recipe description</label>
+                        <textarea placeholder="Enter a brief description" name="description"></textarea>
+
+                        <button type="submit" class="btn">Enter</button>
+                    </form>
+                </div>
+
+
+            <?php
+            }
+            ?>
         </div>
     </div>
 
@@ -68,57 +119,7 @@ try {
 
 </html>
 
-<?php
-// Recupéré session de calendrier
-$req = $bdd->prepare('SELECT id_user FROM Students WHERE email = ?');
-$req->execute([
-    $_SESSION['email']
-]);
-$data = $req->fetch();
 
-$req->closeCursor();
-
-$_SESSION['idUser'] = $data['id_user'];
-
-if (!empty($_POST['title_watch']) && !empty($_POST['date']) && !empty($_POST['description'])) {
-    $req = $bdd->prepare('INSERT INTO watch_recipe (FK_id_user, title_watch, date, description) VALUES (?, ?, ?, ?)') or die(print_r($bdd->errorInfo()));
-    $req->execute([
-        $_SESSION['idUser'],
-        strip_tags(trim($_POST['title_watch'])),
-        strip_tags(trim($_POST['date'])),
-        strip_tags(trim($_POST['description'])),
-    ]);
-    $req->closeCursor();
-?>
-
-    <h1> C'est encoder ;-)</h1>
-
-<?php
-
-} else {
-?>
-
-    <div class="form-popup" id="myForm" style="display:none">
-        <form class="formContent" action="" method="POST">
-            <h1>Add a watch</h1>
-
-            <label for="email">Title</label>
-            <input type="text" placeholder="Title" name="title_watch">
-
-            <label for="date">Date</label>
-            <input type="date" placeholder="Enter Password" name="date">
-
-            <label for="explication">Recipe description</label>
-            <textarea placeholder="Enter a brief description" name="description"></textarea>
-
-            <button type="submit" class="btn">Enter</button>
-        </form>
-    </div>
-
-
-<?php
-}
-?>
 <script src="../js/calendrier.js"></script>
 <script src="../js/recipe.js"></script>
 </body>
